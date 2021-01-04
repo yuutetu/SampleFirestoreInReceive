acee620b23bcbf822fb35eb8df8de28eabe2556a
yuutetu <yuutetu@gmail.com>
2021-01-04 19:09:10 +0900

    Initial Commit

---
+++ SampleFirestoreOnReceive/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SampleFirestoreOnReceive
+//
+//  Created by masayuki.kagae on 2021/01/04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SampleFirestoreOnReceive/SampleFirestoreOnReceiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleFirestoreOnReceiveApp.swift
+//  SampleFirestoreOnReceive
+//
+//  Created by masayuki.kagae on 2021/01/04.
+//
+
+import SwiftUI
+
+@main
+struct SampleFirestoreOnReceiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
